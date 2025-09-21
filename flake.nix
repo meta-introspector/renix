8aeaaa918e20f9f3c4c85c740d6b031b7d7edb01
mike <h@solfunmeme.com>
2025-09-21 03:49:13 +0000

    feat(renix): CRQ-016 - Standardize flake.nix from template

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "A flake for this submodule, providing a basic development shell.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
